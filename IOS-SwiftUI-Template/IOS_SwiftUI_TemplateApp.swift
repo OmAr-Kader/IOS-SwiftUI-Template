ea7643482e51b78839e7d0058a567ac26611f390
OmAr-Kader <ramooor700@gmail.com>
2024-05-18 19:52:05 +0300

    Initial Commit

---
+++ IOS-SwiftUI-Template/IOS_SwiftUI_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_SwiftUI_TemplateApp.swift
+//  IOS-SwiftUI-Template
+//
+//  Created by OmAr on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOS_SwiftUI_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
